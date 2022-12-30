2a7d24739c4eeb9a69a1b180f3622b2b29d09e1e
Ammar <ammar.ahmed9020@gmail.com>
2022-12-30 12:00:31 +0500

    Frog game Updated

--- Frog Jump Game/main.cpp
+++ Frog Jump Game/main.cpp
@@ -1,17 +1,24 @@
 #include <iostream>
+#include <thread>
 #include <conio.h>
 #include <windows.h>
 #include <cstdlib>
 #include <ctime>
 #include <stdlib.h>
+#include <time.h>
+#include <chrono>
+#include <iomanip>
 
 
 using namespace std;
 
+void restart();
+
+
 bool gameOver;
 const int MAX=3;
 char ch[MAX]={'_','+','o'};
-
+int score=0;
 
 int x,y;
 
@@ -22,6 +29,12 @@ struct Node{
     Node *prev;
 };
 Node *start,*l,*temp;
+
+void reset(){
+  start=NULL;
+  l=NULL;
+  temp=NULL;
+}
 
 void blocset(){
     system("cls");
@@ -67,23 +80,29 @@ void setup(){
 
 void blocks(){
      system("cls");
+      for(int i=0;i<10;i++){
+    cout<<endl;
+    }
+    cout<<"\t\t\t\t\t";
       l=start;
     for(int i=0; i<5; i++){
      for(int j=0 ; j<5; j++){
            if(i==y && j==x){
-            cout.width(3);
+            cout.width(5);
+             cout.widen(5);
             cout<<"F";    
               l=l->next;
 
                 }
 
           else{ 
-           
-            cout.width(3);
+            cout.width(5);
+            cout.widen(5);
             cout<<l->data;
             l=l->next;
               if(l->next==NULL){
-              cout.width(4);
+              cout.width(6);
+              cout.widen(5);
               l->prev->data='@';
               
              } 
@@ -92,6 +111,7 @@ void blocks(){
      }
     
       cout<<endl;
+      cout<<"\t\t\t\t\t";
     }
     
     
@@ -102,11 +122,12 @@ void boundary(){
  if(x>4 || x<0 || y>4 || y<0){
     gameOver=true;
  }
-    
+
+  
 }
   
 
-void input(){
+void Input_and_Logic(){
   
   if(_kbhit()){
 
@@ -135,7 +156,6 @@ for(int i=0; i<5; i++){
                 l->prev->data='_'; 
                 
             } 
-            
 
            }
           else{  
@@ -145,7 +165,12 @@ for(int i=0; i<5; i++){
      }
       cout<<endl;
     }
+      if((x>0 && x<24)||(y>0 && y<24)){
+              score+=10;
+            }
         break;
+
+
 
         case 'd':
         x++;
@@ -170,6 +195,7 @@ for(int i=0; i<5; i++){
                 l->prev->data='_'; 
                 
             }
+
            }
           else{  
             l=l->next;  
@@ -179,7 +205,12 @@ for(int i=0; i<5; i++){
      }
       cout<<endl;
     }
+       if((x>0 && x<24)||(y>0 && y<24)){
+              score+=10;
+            }
         break;
+
+
 
         case 'w':
         y--;
@@ -201,12 +232,13 @@ for(int i=0; i<5; i++){
                 l=l->next;
                 l->prev->data=' '; 
                 
+                
             }
           else  if(l->data=='+'){
                 l=l->next;
                 l->prev->data='_'; 
             }
-          
+
            }
           else{  
             l=l->next;
@@ -216,7 +248,13 @@ for(int i=0; i<5; i++){
      }
       cout<<endl;
     }
+       if((x>0 && x<24)||(y>0 && y<24)){
+              score+=10;
+            }
+   
         break;
+
+
 
         case 's':
         y++;
@@ -234,14 +272,13 @@ for(int i=0; i<5; i++){
                } 
               else if(l->data=='_'){
                   l=l->next;
-                l->prev->data=' '; 
-              
-            }
+                l->prev->data=' ';  
+              }
             else if(l->data=='+'){
                l=l->next;
                 l->prev->data='_'; 
+               
             }
-             
                 
            }
           else{  
@@ -253,8 +290,11 @@ for(int i=0; i<5; i++){
      }
       cout<<endl;
     }
+        if((x>0 && x<24)||(y>0 && y<24)){
+              score+=10;
+            }
         break;
-
+     
 
         case '':
        gameOver=true;
@@ -263,24 +303,69 @@ for(int i=0; i<5; i++){
 
   }
 
+  
+}
+
+  
+
+  
+
  
 
+
+
+void sc(){
+cout<<"\n\n\t\t\t\t\tScore: "<<score<<endl;  
 }
+
 
 
 
 int main()
 {
     system("cls");
+    reset();
     blocset(); 
     setup();
      
     while(!gameOver){
     blocks();
-    input();
+    Input_and_Logic();
     boundary();
+    sc();
     }
+    restart();
 
 
     return 0;
 }
+
+void restart(){
+   system("CLS");
+  char ch;
+  for(int i=0;i<10;i++){
+    cout<<endl;
+    }
+     cout<<"\t\t\t\t\t YOUR FINAL SCORE IS = "<<score<<endl;
+     cout<<" \t\t\t\t\t Wanna restart (Y/N) = ";
+     cin>>ch;
+
+     if((ch!='Y') && (ch!='N')){
+       cout<<endl<<"\t\t\t\t\t NOTE: Invalid input enter Y or N ";
+       Sleep(1000);
+       cin.clear();
+       cin.ignore();
+        restart();
+      }
+     switch(ch){
+
+     case 'Y':
+       main();
+        break;
+
+     case 'N':
+        cout<<endl<<"\t\t\t\t\t THANK YOU FOR PLAYING! "<<endl;
+        break;
+     }
+}
+
